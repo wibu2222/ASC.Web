fa843086ebfcc97d13c788e15400cfd07519b840
wibu2222 <haclongkil@gmail.com>
2025-02-27 14:00:47 +0700

    Add project files.

---
+++ ASC.Web/Configuration/ApplicationSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace ASC.Web.Configuration
+{
+    public class ApplicationSettings
+    {
+        public string ApplicationTitle { get; set; }
+    }
+}
---
+++ ASC.Web/Services/IEmailSender.cs
@@ -0,0 +1,8 @@
+﻿namespace ASC.Web.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+        
+    }
+}
